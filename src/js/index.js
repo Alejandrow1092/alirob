7f8e2827ea3ef5f87fc7a412581df74cd9d0f5ba
Alejandro Martínez <amrkratos@hotmail.com>
2022-05-06 16:12:10 -0500

    Slider y el menu de headers

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,33 @@
 //alert("hola")
+
+//se activa junto con la venta al cargar
+
+const img = [
+    "/src/assets/img/imagen1.jpg",
+    "/src/assets/img/imagen2.jpg",
+    "/src/assets/img/imagen3.jpg",
+    "https://picsum.photos/1000/1000"
+];
+
+
+
+window.addEventListener('load', () =>{
+    console.log("el contenido a cargado");
+
+    let indice=0;
+    let tam = img.length - 1;
+
+    const cambiaImg=()=>{
+        console.log(indice)
+        document.getElementById('slider').src = img[indice]
+        if(indice<tam){
+            indice++;
+        }
+        else{
+            indice=0;
+        }
+    }
+
+    setInterval(cambiaImg, 4000)
+    
+})
